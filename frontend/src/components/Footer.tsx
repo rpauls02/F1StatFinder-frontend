59a8fb613f5d7bbe55802decbc62795732b1209b
Robert Pauls <rpauls2002@gmail.com>
2025-09-04 21:03:52 +0100

    Create Footer.tsx

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../assets/css/Footer.css'
+import discord_logo from '../assets/img/discord-white.png';
+import x_logo from '../assets/img/x-white.png';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            {/*<div className="footer-contacts">
+                <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+                    <img src={x_logo} alt="X-logo" style={{  }} />
+                </a>
+                <a href="https://discord.com/" target="_blank" rel="noopener noreferrer">
+                    <img src={discord_logo} alt="discord-logo" style={{  }} />
+                </a>
+            </div>*/}
+
+            <div id="footer-disclaimer">
+                <p>Copyright © 2025 Robert Pauls. All rights reserved</p>
+                {/*<p>
+                    This website is unofficial and is not associated in any way with the Formula 1 companies.
+                    <br/>
+                    F1, FORMULA ONE, FORMULA 1, FIA FORMULA ONE WORLD CHAMPIONSHIP, GRAND PRIX and related marks are
+                    trademarks of Formula One Licensing B.V.
+                </p>*/}
+            </div>
+
+            <div id="footer-sections">
+
+            </div>
+
+
+        </footer>
+    );
+};
+
+export default Footer;
