c5ee3a0526b2293b8f045854ddc5900b2de5ebd7
Robert Pauls <rpauls2002@gmail.com>
2025-09-14 16:17:26 +0100

    Update footer component and styling

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -6,27 +6,23 @@ import x_logo from '../assets/img/x-white.png';
 const Footer: React.FC = () => {
     return (
         <footer>
-            {/*<div className="footer-contacts">
+            <div id="contacts">
                 <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
                     <img src={x_logo} alt="X-logo" style={{  }} />
                 </a>
                 <a href="https://discord.com/" target="_blank" rel="noopener noreferrer">
                     <img src={discord_logo} alt="discord-logo" style={{  }} />
                 </a>
-            </div>*/}
+            </div>
 
-            <div id="footer-disclaimer">
+            <div id="disclaimer">
                 <p>Copyright © 2025 Robert Pauls. All rights reserved</p>
-                {/*<p>
+                <p>
                     This website is unofficial and is not associated in any way with the Formula 1 companies.
                     <br/>
                     F1, FORMULA ONE, FORMULA 1, FIA FORMULA ONE WORLD CHAMPIONSHIP, GRAND PRIX and related marks are
                     trademarks of Formula One Licensing B.V.
-                </p>*/}
-            </div>
-
-            <div id="footer-sections">
-
+                </p>
             </div>
 
 
